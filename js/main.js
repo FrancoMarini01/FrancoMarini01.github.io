0a506a84d704edf2961baf4c9539d1197f2db94e
Franco Marini <francomarinirv@gmail.com>
2022-11-27 19:43:48 -0300

    main js y enlace de script

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let admin = true;
+
+
+
+let productos = parseInt (prompt("CUANTOS PRODUCTOS DESEA COMPRAR?"));
+
+    for (let i = 0; i < productos ; i++) {
+        if (productos === 0){
+            alert ("Siga recorriendo la pagina sin problemas!");
+        } else{
+            alert ("Puede ver nuestras ofertas y promociones!");
+        }
+    }
