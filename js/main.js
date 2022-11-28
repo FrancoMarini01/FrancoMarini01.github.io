aebe36faac7dbf715fabee9bfe4f573628799594
Franco Marini <francomarinirv@gmail.com>
2022-11-28 19:33:04 -0300

    correccion JS

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
-let admin = true;
+let admin = (prompt("Coloque 1 si usted es administrador. En su defecto, 2 si usted es cliente"));
 
+if (admin == 1){
+    (alert(`Ingrese desde la base de datos por favor.`));
+} else if (admin != 1){
+    (alert("puede seguir comprando!"));
+}
 
 
 let productos = parseInt (prompt("CUANTOS PRODUCTOS DESEA COMPRAR?"));
