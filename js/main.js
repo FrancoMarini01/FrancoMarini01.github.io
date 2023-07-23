b4bdfb8b67b9d1c18fe00736d362160a86ff0f14
Franco Marini <francomarinirv@gmail.com>
2023-07-23 15:34:40 -0300

    preentrega230723

--- js/main.js
+++ js/main.js
@@ -5,7 +5,17 @@ let cliente = parseInt(prompt("si usted es cliente escriba 1. Si es administrado
         alert("Digite clave de administrador");
     }
 
+let pass = 1234;
+let dato = parseInt(prompt("cual es la clave?"));
+let intentos = 3;
 
+while (dato != pass && intentos > 0 ) {
+    intentos--;
+     alert(`la clave es incorrecta, te quedan ${intentos} intentos`);
+   if (intentos != 0) {
+        dato = parseInt(prompt("cual es la clave?"));
+    }
+}
 
 
 
