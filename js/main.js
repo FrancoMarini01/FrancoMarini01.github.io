58b8f18d3abb74a1d1046791494ec2ecb31a3959
FrancoMarini01 <francomarinirv@gmail.com>
2023-08-09 00:36:50 -0300

    2daPreentrega

--- js/main.js
+++ js/main.js
@@ -1,24 +1,24 @@
 // -----------Preentrega 1 ------------------
 
-// let admin = (prompt("Coloque 1 si usted es administrador. En su defecto, 2 si usted es cliente"));
+let administrador = (prompt("Coloque 1 si usted es administrador. En su defecto, 2 si usted es cliente"));
 
-// if (admin == 1){
-//     (alert(`Ingrese desde la base de datos por favor.`));
-// } else if (admin != 1){
-//     (alert("puede seguir comprando!"));
-// }
+if (administrador == 1){
+    (alert(`Ingrese desde la base de datos por favor.`));
+} else if (administrador != 1){
+    (alert("puede seguir comprando!"));
+}
 
-// let pass = 1234;
-// let dato = parseInt(prompt("cual es la clave?"));
-// let intentos = 3;
+let pass = 1234;
+let dato = parseInt(prompt("cual es la clave?"));
+let intentos = 3;
 
-// while (dato != pass && intentos > 0 ) {
-//     intentos--;
-//      alert(`la clave es incorrecta, te quedan ${intentos} intentos`);
-//    if (intentos != 0) {
-//         dato = parseInt(prompt("cual es la clave?"));
-//     }
-// }
+while (dato != pass && intentos > 0 ) {
+    intentos--;
+     alert(`la clave es incorrecta, te quedan ${intentos} intentos`);
+   if (intentos != 0) {
+        dato = parseInt(prompt("cual es la clave?"));
+    }
+}
 
 // ---------------Preentrega 2 -------------------
 
